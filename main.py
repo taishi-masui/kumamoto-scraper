3ea48435e4da7db8494ec9ffa0341a52d619645e
taishi-masui <t-masui@sukima-correct.com>
2026-03-23 21:47:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("Hello GitHub Actions")
+from playwright.sync_api import sync_playwright
+
+def main():
+    with sync_playwright() as p:
+        browser = p.chromium.launch()
+        page = browser.new_page()
+
+        page.goto("https://example.com")
+
+        print(page.title())
+
+        browser.close()
+
+if __name__ == "__main__":
+    main()
